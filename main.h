4329b2bd726dcadd98375910a2dd0b9a25123331
Divine-bright <brightheybukah@gmail.com>
2022-04-26 05:23:10 -0700

    task 0

--- main.h
+++ main.h
@@ -1,23 +1,15 @@
-#ifndef MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#define MAIN_H
-
-
-
-#include <stdio.h>
-
-
-
-void positive_or_negative(int i);
-
-int largest_number(int a, int b, int c);
-
-void print_remaining_days(int month, int day, int year);
-
-int convert_day(int month, int day);
-
-void print_times_table(int n);
-
-
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
